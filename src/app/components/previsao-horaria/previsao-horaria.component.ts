7a0c073b562a2da241cd5de81b6bc4bed14114ba
Vinicius  Rodrigues <viniciusgattirodrigues2003@gmail.com>
2025-11-12 09:32:43 -0300

    Adiciona PrevisaoHorariaComponent e completa exibição de dados

--- src/app/components/previsao-horaria/previsao-horaria.component.ts
+++ src/app/components/previsao-horaria/previsao-horaria.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'; 
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-previsao-horaria',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './previsao-horaria.component.html',
   styleUrl: './previsao-horaria.component.scss'
 })
 export class PrevisaoHorariaComponent {
-
+  @Input() hourlyData: any;
 }
